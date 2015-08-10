59eccb32832bb0434c4622f7f394b05920543bb6
Sonja Krause-Harder <skh@rcane.de>
2015-08-11 01:03:15 +0200

    Switch on G+ login

--- settings.py
+++ settings.py
@@ -12,5 +12,5 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '714158689173-tcko9c28cnqgfr9mkjvrsspkui78bmr9.apps.googleusercontent.com'
 
--- static/js/app.js
+++ static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '714158689173-tcko9c28cnqgfr9mkjvrsspkui78bmr9.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
